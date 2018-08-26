7a364d2344f90ef7aebd2aaafbd2ca8fb8f34d97
Samuel E. Reyes Lara <sreyes.sw@gmail.com>
2018-08-26 18:38:34 -0300

    Correccion migracion
    CreateCableRadiantesTable
    
    Se actualiza nombre de clase interna.
    
    Signed-off-by: Samuel E. Reyes Lara <sreyes.sw@gmail.com>

--- database/migrations/2018_08_26_181942_create_cable_radiantes_table.php
+++ database/migrations/2018_08_26_181942_create_cable_radiantes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCablesRadiantesTable extends Migration
+class CreateCableRadiantesTable extends Migration
 {
 	/**
 	 * Run the migrations.
