c5ff5db33f1757392d28f43b6853ebab46195f29
Samuel E. Reyes Lara <sreyes.sw@gmail.com>
2018-10-25 14:13:50 -0300

    Imports y Exports
    Signed-off-by: Samuel E. Reyes Lara <sreyes.sw@gmail.com>

---
+++ app/Exports/UAsignadosExport.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Exports;
+
+use App\Usuario;
+use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+
+class UAsignadosExport implements FromQuery, WithMapping, WithHeadings, ShouldAutoSize
+{
+	use Exportable;
+
+	public function query()
+	{
+		return Usuario::with('rol');
+	}
+
+	public function map($usuario): array
+	{
+		return [
+			$usuario->rut,
+			$usuario->nombre,
+			$usuario->apPaterno,
+			$usuario->apMaterno,
+			$usuario->mail,
+			$usuario->rol->nombre
+		];
+	}
+
+	public function headings(): array
+	{
+		return [
+			'rut',
+			'nombre',
+			'apPaterno',
+			'apMaterno',
+			'mail',
+			'nombre'
+		];
+	}
+}
